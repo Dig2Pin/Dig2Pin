c4bcf94217c267f9753033e76f80aa6b342c9766
Huanry <Huanry@MacBook-Air-1470.local>
2020-05-27 23:08:34 +0800

    add comment

--- data/Comment.js
+++ data/Comment.js
@@ -1,1 +1,36 @@
-//comment on links, pins or comments
+const {
+  Text,
+  Relationship,
+  DateTime,
+} = require('@keystonejs/fields');
+
+const { AuthedRelationship } = require('@keystonejs/fields-authed-relationship');
+
+const {Url} = require('./Url.js');
+const {User} = require('./User.js');
+
+
+
+const isAdmin = ({ authentication: { item: user } }) => !!user && !!user.isAdmin;
+
+exports.Comment = {
+  fields: {
+    body: { type: Text, isMultiline: true },
+    url: {
+      type: Relationship,
+      ref: 'Url',
+    },
+    author: {
+      type: AuthedRelationship,
+      ref: 'User',
+      isRequired: true,
+      access: {
+        create: isAdmin,
+        update: isAdmin,
+      },
+    },
+    posted: { type: DateTime },
+  },
+  labelResolver: item => item.body,
+};
+

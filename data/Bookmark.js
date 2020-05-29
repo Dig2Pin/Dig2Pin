fc9a91a63690c54080e49d9a9b7fc14596ca4699
Huanry <Huanry@MacBook-Air-1470.local>
2020-05-29 21:00:02 +0800

    add bookmark

--- data/Bookmark.js
+++ data/Bookmark.js
@@ -1,1 +1,32 @@
-//to bookmark links
+const {
+	Text,
+	Relationship,
+	DateTime,
+} = require('@keystonejs/fields');
+
+const { AuthedRelationship } = require('@keystonejs/fields-authed-relationship');
+
+const {User} = require('./User.js');
+
+
+
+const isAdmin = ({ authentication: { item: user } }) => !!user && !!user.isAdmin;
+
+exports.Bookmark = {
+	fields: {
+		title:{ type: Text },
+		description: { type: Text },
+		owner: {
+			type: AuthedRelationship,
+			ref: 'User',
+			isRequired: true,
+			access: {
+				create: isAdmin,
+				update: isAdmin,
+			},
+		},
+		created: { type: DateTime },
+	},
+	labelResolver: item => item.owner,
+};
+

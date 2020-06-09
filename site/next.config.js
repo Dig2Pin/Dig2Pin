b23c2471663256cde921f127fd74d5d86fa54561
Huanry <Huanry@MacBook-Air-1470.local>
2020-06-09 11:59:42 +0800

    Update next.config.js
    for initApollo.js file

--- site/next.config.js
+++ site/next.config.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
+
+const serverUrl = process.env.SERVER_URL || 'http://localhost:3000';
+
 const { distDir } = require('../config');
 
 module.exports = {
   distDir: `../${distDir}/www`,
   devIndicators: {
     autoPrerender: false,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    serverUrl,
   },
 };

6db82fb60f4933125fb7c752b6354e8f2869c15f
Huanry <Huanry@MacBook-Air-1470.local>
2020-06-13 21:55:01 +0800

    Update index.js
    recommad lasted pinned
     add button to load

--- site/pages/index.js
+++ site/pages/index.js
@@ -5,6 +5,7 @@ import { useQuery } from '@apollo/react-hooks';
 
 import { jsx } from '@emotion/core';
 import { format, parseISO } from 'date-fns';
+import { useState } from 'react';
 
 import Layout from '../templates/layout';
 import Header from '../components/header';
@@ -22,9 +23,88 @@ const GET_AUTH =
     }
     `;
 
-
+const GET_URL =
+  gql`
+      query GetUrl($count:Int!){
+        allUrls(first:$count,sortBy: posted_DESC){
+          slug
+          title
+          url
+          description
+          posted
+          author{userName}
+        }
+      }
+  `
 
 /** @jsx jsx */
+
+const Pined = () => {
+  const { data, loading, error } = useQuery(gql`
+    query {
+      allPins(first: 3, sortBy: created_DESC){
+        title
+        body
+        url{
+          slug
+        }
+        bookmark{
+          owner{
+            userName
+          }
+        }
+      }
+    }
+  `);
+
+  if (loading){
+    return(<h1>Loding Pin</h1>)
+  }
+  if(error){
+    return(<h1>Err</h1>)
+  }
+
+ return(
+  <>
+  <style dangerouslySetInnerHTML={{__html: "@media (max-width: 580px) { .recom { width:98%;padding-bottom:49%}}\n@media (min-width: 580px) and (max-width: 900px) { .recom { width:47%;padding-bottom:24%}} " }} />
+    {data.allPins.length
+      ? (<div>
+        <h2>Latest Pinned</h2>
+        {data.allPins.map(p => (
+          <div
+            key={p.id}
+            className ='recom'
+            css={{
+              width: '31%',
+              height:0,
+              paddingBottom:'15%',
+              float:'left',
+              background:'white',
+              borderRadius: 6,
+              marginBottom:'2%',
+              marginRight:'1%',
+              marginLeft:'1%',
+              boxShadow: '0px 10px 20px hsla(200, 20%, 20%, 0.20)',
+              overflow: 'hidden',
+            }}
+          >
+          <Link href={`/post/${p.url.slug}`}>
+            <a target="_blank" style={{textDecoration: 'none'}}>
+              <div style={{margin:'1em'}}>
+                <p style={{color: 'hsl(200, 20%, 50%)',borderBottom: '1px solid hsl(200, 20%, 80%)'}} >Pinned By: {p.bookmark.owner.userName}</p>
+                <h5 style={{color: '#29363D',marginTop:'-10px'}}>{p.title}</h5>
+                <p style={{color: 'hsl(200, 20%, 50%)'}} >{p.body}</p>
+              </div>
+            </a>
+          </Link>
+          </div>
+        ))}
+       </div>): (null)
+    }
+            <div style={{clear:'both'}}></div>
+  </>
+  )
+}
 
 const Post = ({ post }) => {
   return (
@@ -59,18 +139,10 @@ const Post = ({ post }) => {
 };
 
 export default () => {
-  const { data, loading, error } = useQuery(gql`
-    query {
-      allUrls(sortBy: posted_DESC){
-        slug
-        title
-        url
-        description
-        posted
-        author{userName}
-      }
-    }
-  `);
+
+  const [count, setCount] = useState(20);
+
+  const { data, loading, error } = useQuery(GET_URL,{ variables: {count}});
 
     const {
     data: { authenticatedUser = [] } = {},
@@ -83,24 +155,30 @@ export default () => {
       <Header />
       <nav style={{backgroundColor: 'orange', padding: '0.5rem', marginTop: '2rem', boxShadow: '0px 10px 20px hsla(200, 20%, 20%, 0.20)', borderRadius: '6px'}}>
         <ul className="nav" >
-            <li className="nav-item">
-              <Link href={`/`}>
-                <a style={{color: 'orange', backgroundColor: '#eee', borderRadius: '6px', padding:'5px',margin:'5px'}}>
-                Home
-                </a>
-              </Link>
-            </li>
-            {
-              authenticatedUser? (
-                  <li className="nav-item">
-                    <Link href={`/bookmark/${authenticatedUser.slug}`}>
-                      <a style={{color: '#eee', borderRadius: '6px', padding:'5px',margin:'5px'}}>
-                      Bookmarks
-                      </a>
-                    </Link>
-                  </li>
-                ):(null)
+          <li className="nav-item">
+            <Link href={`/`}>
+              <a style={{color: 'orange', backgroundColor: '#eee', borderRadius: '6px', padding:'5px',margin:'5px'}}>
+              Home
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item">
+          {
+            authenticatedUser? (
+                  <Link href={`/bookmark/${authenticatedUser.slug}`}>
+                    <a style={{color: '#eee', borderRadius: '6px', padding:'5px',margin:'5px'}}>
+                    Bookmarks
+                    </a>
+                  </Link>
+              ):(
+                  <Link href={`/bookmark`}>
+                    <a style={{color: '#eee', borderRadius: '6px', padding:'5px',margin:'5px'}}>
+                    Bookmarks
+                    </a>
+                  </Link>
+              )
             }
+          </li>
         </ul>
       </nav>
       <section css={{ margin: '48px 0' }}>
@@ -109,7 +187,9 @@ export default () => {
         and comment interesting instant links in your friend circle. Check our github page to join us.
         </p>
       </section>
-
+      <section>
+      <Pined/>
+      </section>
       <section css={{ margin: '48px 0' }}>
         <h2>Latest Posts</h2>
         {loading ? (
@@ -125,6 +205,23 @@ export default () => {
             )}
           </div>
         )}
+      </section>
+      <section>
+        <center>
+          <button
+            css={{
+              padding: '6px 12px',
+              borderRadius: 6,
+              background: 'black',
+              fontSize: '1.5em',
+              color: 'white',
+              border: 0,
+            }}
+            onClick={() => setCount(count + 20)}
+          >
+            Load More
+          </button>
+        </center>
       </section>
     </Layout>
   );

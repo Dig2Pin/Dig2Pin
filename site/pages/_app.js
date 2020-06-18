69694cb759081113dead45babcb41c64c46e540d
Huanry <Huanry@MacBook-Air-1470.local>
2020-06-18 15:00:36 +0800

    Update _app.js

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -14,7 +14,7 @@ const MyApp = ({ Component, pageProps, apolloClient, user }) => {
         <AuthProvider initialUserValue={user}>
           <Head>
             <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-            <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
             <meta
               name="viewport"
               content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
